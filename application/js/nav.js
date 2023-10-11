72512c61914d3bbbfedc264c31b14161a1de179a
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-10-11 21:58:26 +0800

    update

--- application/js/nav.js
+++ application/js/nav.js
@@ -21,4 +21,23 @@ $(function() {
         if(screenWidth > 910) nav.css("height","60px");
         console.log(screenWidth);
     });
+
+    let lastScrollTop = 0;
+
+    $(window).scroll(() => {
+        let scrollTop = $(this).scrollTop();
+    
+        if (scrollTop > lastScrollTop && scrollTop > 60) {
+            nav.fadeOut(300, function() {
+                nav.css("display", "none");
+            });
+        } else {
+            nav.fadeIn(300, function() {
+                nav.css("display", "flex");
+            });
+        }
+    
+        lastScrollTop = scrollTop;
+    });
+    
 });
