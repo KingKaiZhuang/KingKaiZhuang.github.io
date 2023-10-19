40059c4a19de7eaa8d37ce24b5ef9800b9e6101c
KingKaiZhuang <5b0g0024@stust.edu.tw>
2023-10-19 18:45:05 +0800

    update

--- application/js/main.js
+++ application/js/main.js
@@ -4,7 +4,10 @@ import { CountUp } from './countup.js';
 
 $(document).ready(function() {
   var countUp; // 建立 CountUp 實例
-
+  var flag1 = 0;
+  var flag2 = 0;
+  var flag3 = 0;
+  var flag4 = 0;
   // 當使用者滾動到特定標籤時觸發
 $(window).scroll(function() {
   var elementInView1 = isElementInViewport('#target1');
@@ -12,24 +15,28 @@ $(window).scroll(function() {
   var elementInView3 = isElementInViewport('#target3');
   var elementInView4 = isElementInViewport('#target4');
 
-  if ((elementInView1) && !countUp) {
+  if ((elementInView1) && !countUp && flag1 == 0) {
     countUp = new CountUp('target1', 80, { suffix: '%' });
     countUp.start();
+    flag1 = 1;
     countUp = undefined;
   }
-  if ((elementInView2) && !countUp) {
+  if ((elementInView2) && !countUp && flag2 == 0) {
     countUp = new CountUp('target2', 65, { suffix: '%' });
     countUp.start();
+    flag2 = 1;
     countUp = undefined;
   }
-  if ((elementInView3) && !countUp) {
+  if ((elementInView3) && !countUp && flag3 == 0) {
     countUp = new CountUp('target3', 70, { suffix: '%' });
     countUp.start();
+    flag3 = 1;
     countUp = undefined;
   }
-  if ((elementInView4) && !countUp) {
+  if ((elementInView4) && !countUp && flag4 == 0) {
     countUp = new CountUp('target4', 60, { suffix: '%' });
     countUp.start();
+    flag4 = 1;
   }
 });
 
